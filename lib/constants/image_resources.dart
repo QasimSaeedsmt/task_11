1444973e34d07550d77488a54b77a7cb56d467be
Qasim Saeed <qasim.saeed@techswivel.com>
2024-01-15 16:19:01 +0500

    remove unwanted files

--- lib/constants/image_resources.dart
+++ lib/constants/image_resources.dart
@@ -4,22 +4,4 @@ class ImageResources {
   static const String RANDOM_PICTURE = "assets/images/kfc.jpg";
   static const String PERSON_IMAGE = "assets/images/person.jpg";
   static const String KFC2 = "assets/images/kfc2.jpg";
-  static const List<String> LIST_OF_IMAGES = [
-    "assets/images/tour_image_1.png",
-    "assets/images/tour_image_2.png",
-    "assets/images/tour_image_2.png",
-    "assets/images/tour_image_1.png",
-    "assets/images/tour_image_1.png",
-    "assets/images/tour_image_1.png",
-    "assets/images/tour_image_1.png",
-    "assets/images/tour_image_2.png",
-    "assets/images/tour_image_1.png",
-  ];
-  static const List<String> LIST_OF_PACKING_IMAGES = [
-    "assets/images/packing_image1.png",
-    "assets/images/packing_image2.png",
-    "assets/images/packing_image1.png",
-    "assets/images/packing_image4.png",
-    "assets/images/packing_image5.png",
-  ];
 }
